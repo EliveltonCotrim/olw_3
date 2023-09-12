b22308ac48f9908038f8aa15e12db761a5971153
Elivelton <elivelton.gbi@gmail.com>
2023-09-11 23:32:36 -0300

    Modelacão: Model, migration, enums, Factory, seeds

---
+++ app/Models/Shipping.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Shipping extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'district',
+        'number',
+        'complement',
+        'tracking_code',
+        'status',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
